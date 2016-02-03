932697aeebb1aa9b1cdae7d9a20ee4c0ead2279c
James Ralphs <jr39.dev@gmail.com>
2016-02-03 08:55:13 +0100

    nicer search bar, show URLs

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ mainApp.factory("MockBookmarksFactory", function() {
 	return function() {
 		return {
 			fetch: function(callback, context) {
-				var data = [{
-						url: "mockurl",
-						title: "mocktitle"
-					}];
+				var data = {
+					bookmarks: [{
+							url: "mockurl",
+							title: "mocktitle"
+						}]
+				};
 				callback.call(context || this, data);
 			}
 		};
@@ -16,11 +18,77 @@ mainApp.factory("MockBookmarksFactory", function() {
 
 mainApp.factory("ChromeBookmarksFactory", function() {
 	return function() {
+
+		/**
+		 * Takes a list of bookmarks and folders and returns a modified list of the bookmarks.
+		 * Produces an additional property, <code>folders</code>, a list of names of folders the bookmark appears in.
+		 * This is populated top to bottom, so the folder the bookmark is immeadiately in would be index 0, and it's folder would be at index 1, etc.
+		 * @param {array} bookmarks list of Chrome bookmarks
+		 * @param {array} folders list of folders
+		 * @returns {array} list of bookmarks with a new <code>folders</code> property
+		 */
+		function mergeFoldersIntoBookmarks(bookmarks, folders) {
+			return _.map(bookmarks, function(bookmark) {
+				var parentFolder,
+						parentFolders = [];
+				(function setFolders(node) {
+					if (node.hasOwnProperty("parentId")) {
+						// todo - performance, does this need to be moved to a collection
+						parentFolder = _.find(folders, function(folder) {
+							return folder.id === node.parentId;
+						});
+						if (parentFolder.title !== "" && parentFolder.title !== "Bookmarks bar") {
+							parentFolders.push(parentFolder.title);
+						}
+						setFolders(parentFolder);
+					}
+				})(bookmark);
+
+				return _.extend(bookmark, {
+					folders: parentFolders
+				});
+			});
+		};
+
+
+		/**
+		 * Flattens a bookmark structure from Chrome <code>bookmarks.getTree</code> API.
+		 * Converts into a map with the following two keys:
+		 * <ul>
+		 * <li><code>bookmarks</code> list of all the bookmarks</li>
+		 * <li><code>folders</code> list of the folders</li>
+		 * </ul>
+		 * @param {object|array} bookmarkSubTree Chrome <code>bookmarks.getTree</code> API callback passed-in parameter
+		 * @returns {object} map with <code>bookmarks</code> and <code>folders</code> properties, both of which are lists.
+		 */
+		function flatten(bookmarkSubTree) {
+			var bookmarks = [],
+					folders = [];
+			(function flatten(bookmarkSubTree) {
+				if (Array.isArray(bookmarkSubTree)) {
+					return bookmarkSubTree.map(function(bookmarkSubTreeChild) {
+						return flatten(bookmarkSubTreeChild);
+					});
+				} else if (bookmarkSubTree.hasOwnProperty("children")) {
+					folders.push(bookmarkSubTree);
+					return flatten(bookmarkSubTree.children);
+				} else {
+					// don't accept bookmarklets
+					if (bookmarkSubTree.url.indexOf("javascript:") !== 0) {
+						bookmarks.push(bookmarkSubTree);
+					}
+				}
+			})(bookmarkSubTree);
+			return {
+				bookmarks: bookmarks,
+				folders: folders
+			};
+		};
+		
 		return {
 			fetch: function(callback, context) {
 				chrome.bookmarks.getTree(function(bookmarkTree) {
-					console.log(bookmarkTree);
-					callback.call(context || this, bookmarkTree);
+					callback.call(context || this, flatten(bookmarkTree));
 				});
 			}
 		};
@@ -28,8 +96,15 @@ mainApp.factory("ChromeBookmarksFactory", function() {
 });
 
 mainApp.controller('bookmarksController', function($scope, MockBookmarksFactory, ChromeBookmarksFactory) {
-	bookmarks = window.chrome.bookmarks ? new ChromeBookmarksFactory() : new MockBookmarksFactory();
+	var bookmarks = window.chrome.bookmarks ? new ChromeBookmarksFactory() : new MockBookmarksFactory();
 	bookmarks.fetch(function(bookmarks) {
-		$scope.bookmarks = bookmarks;
+		$scope.bookmarks = bookmarks.bookmarks;
+					console.log($scope.bookmarks);
 	}, this);
+	
+	$scope.openLinkInNewTab = function(event){
+		console.log(event);
+		return false;
+	};
+	
 });
