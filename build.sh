39a32f01ceb022dee5829e98f45a6599629339e4
James Ralphs <jr39.dev@gmail.com>
2017-05-03 20:29:57 +0200

    bootstrap fonts and requirejs

--- build.sh
+++ build.sh
@@ -6,8 +6,18 @@ rm -r dist
 mkdir dist
 cp src/index.html dist/
 cp src/manifest.json dist/
+
+# bootstrap
 mkdir -p dist/libs/bootstrap/dist/css
+cp -r src/libs/bootstrap/dist/fonts dist/libs/bootstrap/dist/
 cp src/libs/bootstrap/dist/css/bootstrap.css dist/libs/bootstrap/dist/css/bootstrap.css
+
+# icons
 cp -r src/icons dist/
 
+# requirejs
+mkdir -p dist/libs/requirejs
+cp src/libs/requirejs/require.js dist/libs/requirejs/
+
+# bundle
 grunt requirejs:compile
